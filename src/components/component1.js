5f22e0542ceaa30ecfeaa315cdd8abe61ff989fc
Roshan panda <roshanpanda666@gmail.com>
2023-02-11 20:40:09 +0530

    pushing

---
+++ src/components/component1.js
@@ -0,0 +1,27 @@
+import "./component1.css";
+function ExpenceItem(props) {
+
+  const Month=props.date.toLocaleString('en-US',{month:'long'})
+  const Day= props.date.toLocaleString('en-US',{day:'2-digit'})
+  const Year= props.date.getFullYear()
+  return (
+    <div className="parrentt">
+      <div className="dt">
+        <div>{Month}</div>
+        <div>{Day}</div>
+        <div>{Year}</div>
+      </div>
+      <div className="parrent2">
+
+        <h2 className="carinc">{props.title}</h2>
+        <div className="rs">
+
+          <div>rs.{props.amount}</div>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenceItem;
